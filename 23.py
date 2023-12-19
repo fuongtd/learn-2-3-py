a90d3be616fdbc0a7e225b41d19a002a501bc791
fuongtd <fuongtd@users.noreply.github.com>
2023-12-19 12:08:30 +0700

    Update 23.py

--- 23.py
+++ 23.py
@@ -1,11 +1,8 @@
 import datetime
 
-# dt now
 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# to README.md
 text_to_write = f"Updated on {current_time}."
 
-# open > w README.md 
-with open("README.md", "w") as file:
-    file.write(text_to_write)
+with open("window.py", "a") as file:
+    file.write(text_to_write + "\n")
